31b078be463843391e5ced8b58181d4f71c20def
Servio <servio19@gmail.com>
2016-03-13 21:43:24 -0400

    minor changes to schema

--- models/users.js
+++ models/users.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
-
 var UserSchema = new Schema({
   username: {
     type: String,
@@ -13,18 +8,10 @@ var UserSchema = new Schema({
     required: true,
     unique: true
   },
-  password: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: 'Email address is required',
-    validate: [validateEmail, 'Please fill a valid email address'],
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-  }
+  comments: [{
+      type: Schema.ObjectId,
+      ref: 'Comment'
+  }]
 });
 
 var User = mongoose.model('User', UserSchema);
