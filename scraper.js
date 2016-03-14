53c163e540d8bb8f7a052a7e4959d008719a5631
Servio <servio19@gmail.com>
2016-03-13 22:10:10 -0400

    set up scraper, sbut not sure what website to scrape from yet

--- scraper.js
+++ scraper.js
@@ -4,3 +4,39 @@ var request = require('request');
 var cheerio = require('cheerio');
 var PORT = process.env.PORT || 3000;
 
+//Database Config
+var mongoose = require('mongoose');
+//finish db setup
+
+
+app.get('/scraper', function(req, res) {
+      request(<!-- URL LINK -->, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          $ = cherrio.load(body);
+
+          $('title').each(function(i, element) {
+
+              var title = $(this).children("a").text();
+              var link = $(this).children("a").attr("href");
+              //this is an example, your site will be different//
+
+              if (title && link) {
+                    db.scraedData.save({
+                          title: title,
+                          link: link
+                    }, function (err, saved) {
+                          if(err){
+                            console.log(err);
+                          } else {
+                            console.log(saved);
+                          }
+                    })
+              }
+          })
+        }
+      })
+
+      res.send("Scrape Done!");
+});
+
+
