cb2b3ef8a5dd5014378b50dd8e38b767cf3ceff2
Servio <servio19@gmail.com>
2016-03-15 00:11:32 -0400

    successfully able to scrape datagit add .

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var Articles = require('../models/articles.js');
-var mongoose = require('mongoose');
+var request = require('request');
+var cheerio = require('cheerio');
+
+
+var Artice = require('../models/articles.js');
 
 
 router.get('/', function(req, res) {
@@ -9,9 +12,37 @@ router.get('/', function(req, res) {
 });
 
 
+//gets all scraped data
+// router.get('/all', function(req, res) {
+//   res.json(documents);
+// });
 
 
 
+router.get('/scraper', function(req, res) {
+      request('http://www.foxsports.com/nba', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body);
+
+          // $ = cherrio.load(body);
+
+          // $('title').each(function(i, element) {
+
+          //     var img = ;
+          //     var title = $(this).children("a").text();
+          //     var link = $(this).children("a").attr("href");
+          //     var publishInfo = ;
+          //     //this is an example, your site will be different//
+
+          //     if (title && link) {
+
+          //     }
+          // })
+        }
+      })
+
+      res.send("Scrape Done!");
+});
 
 
 module.exports = router;
