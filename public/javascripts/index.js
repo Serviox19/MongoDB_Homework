a839b3df889c9354e90bf423efb2239ff0e482d8
Servio <servio19@gmail.com>
2016-03-15 20:14:32 -0400

    set up structure to populate data to page from database

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,3 +7,30 @@ $(document).ready(function() {
   });
 });
 
+{{!-- <div class="row">
+        <div class="divider"></div>
+        <div class="section">
+          <img src="">
+          <h5>Article Title</h5>
+          <div class="content">
+            <p>Content goes here</p>
+          </div>
+          <div class="inputComment">
+            <form class="col s12" action="" method="POST">
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">comment</i>
+                  <input id="icon_prefix" type="text" class="validate">
+                  <label for="icon_prefix">Type Comment Here!</label>
+                </div>
+              </div>
+            </form><!-- End Form -->
+          </div><!-- End Comment Input -->
+          <div class="Comments">
+            <div class="container">
+            <h6>Comments go here!</h6>
+            </div>
+          </div><!-- End Display Comments section -->
+        </div><!-- End Section -->
+      </div><!-- End Row --> --}}
+
