1c16a65935ade22fbaa977707d217f01f6c3b119
Servio <servio19@gmail.com>
2016-03-17 08:40:32 -0400

    file system update

--- routes/index.js
+++ routes/index.js
@@ -17,52 +17,40 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/comments', function(req, res) {
 
-//gets all scraped data
-// router.get('/articles', function(req, res) {
-//   Article.find({}, function(err, articles) {
-//     if(err) {
-//       throw err;
-//     }
-//     res.json(articles);
-//   })
-// });
+});
 
 
 router.get('/scraper', function(req, res) {
-      request('http://www.foxsports.com/nba', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+  request('http://www.foxsports.com/nba', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
 
-          $ = cheerio.load(body);
+      $ = cheerio.load(body);
 
-          $('.buzzer-article').each(function(i, element) {
-              // console.log(element.children);
-              var img = $(element).children().find('.responsive-image-wrapper').find('img').attr("src");
-              var title = $(element).children().find('.buzzer-title').text();
-              var link = $(element).children().find('.buzzer-title-link').attr('href');
-              var publishInfo = $(element).children().find('.buzzer-details').find('.buzzer-pubdate').text();
+      $('.buzzer-article').each(function(i, element) {
+        var img = $(element).children().find('.responsive-image-wrapper').find('img').attr("src");
+        var title = $(element).children().find('.buzzer-title').text();
+        var publishInfo = $(element).children().find('.buzzer-details').find('.buzzer-pubdate').text();
+        var link = $(element).children().find('.buzzer-title-link').attr('href');
 
-              var article = new Article({
-                image: img,
-                title: title,
-                publishInfo: publishInfo,
-                link: link
-              });
+        var article = new Article({
+          image: img,
+          title: title,
+          publishInfo: publishInfo,
+          link: link
+        });
 
-              article.save(function(err, document){
-                if(err) {
-                  return res.send("ERROR: " + err);
-                };
-              })
-
-              // if (img && title && link && publishInfo);
-
-          })
-        }
-      })
-
-      res.send("Scrape Done!");
-});
+        article.save(function(err, document){
+          if(err) {
+            return res.send("ERROR: " + err);
+          };
+        });
+      });
+    }//end if statement
+  });//end request
+  res.send("Scrape Done!");
+});//end get route
 
 
 
