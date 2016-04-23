8bd49e26d66f8cc7a5d07b8149a35178743524a2
Servio Mora <servio.mora19@gmail.com>
2016-04-23 16:59:57 -0400

    set up code to scrape data for each sport

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
 var Basketball = require('../models/basketball.js');
-// var Football = require('../models/football.js');
-// var Baseball = require('../models/baseball.js');
+var Football = require('../models/football.js');
+var Baseball = require('../models/baseball.js');
 
 
 router.get('/', function(req, res, next) {
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 });
 
 
+//Basketball
 router.get('/basketball', function(req, res, next) {
   request('https://www.foxsports.com/nba', function (err, response, body) {
     if (!err && response.statusCode == 200) {
@@ -50,38 +51,80 @@ router.get('/basketball', function(req, res, next) {
 });//end get route
 
 
-// router.get('/football', function(req, res, next) {
-//   request('https://www.foxsports.com/nfl', function(error, response, body) {
-//     if (!error && response.statusCode == 200) {
+//Football
+router.get('/football', function(req, res, next) {
+  request('https://www.foxsports.com/nfl', function(error, response, body) {
+    if (!err && response.statusCode == 200) {
 
-//       $ = cheerio.load(body);
+      $ = cheerio.load(body);
 
-//       $// start definig data to scrape
-//     }
-//   });// end request
-//   mongoose.model('Football').find(function(err, docs) {
-//     if (!err) {
-//       res.send(docs);
-//     }
-//   });
-// });// end get route
+      $('.buzzer-article').each(function(i, element) {
+        var img = $(element).children().find('.responsive-image-wrapper').find('img').attr("src");
+        var title = $(element).children().find('.buzzer-title').text();
+        var publishInfo = $(element).children().find('.buzzer-details').find('.buzzer-pubdate').text();
+        var link = $(element).children().find('.buzzer-title-link').attr('href');
+
+        var nflArticle = new Football({
+          image: img,
+          title: title,
+          publishInfo: publishInfo,
+          link: link
+        });
+
+        nflArticle.save(function(err, document){
+          if(err) {
+            return res.send("ERROR: " + err);
+          } else {
+            console.log("nba!")
+          }
+        });
+      });
+    }// end if statement
+  });// end request
+  mongoose.model('Football').find(function(err, docs) {
+    if (!err) {
+      res.send(docs);
+    }
+  });
+});// end get route
 
 
-// router.get('/baseball', function(req, res, next) {
-//   request('https://www.foxsports.com/mlb', function(error, response, body) {
-//     if (!error && response.statusCode == 200) {
+////Baseball
+router.get('/baseball', function(req, res, next) {
+  request('https://www.foxsports.com/mlb', function(error, response, body) {
+    if (!err && response.statusCode == 200) {
 
-//       $ = cheerio.load(body);
+      $ = cheerio.load(body);
 
-//       $// start definig data to scrape
-//     }
-//   });// end request
-//   mongoose.model('Baseball').find(function(err, docs) {
-//     if (!err) {
-//       res.send(docs);
-//     }
-//   });
-// });// end get route
+      $('.buzzer-article').each(function(i, element) {
+        var img = $(element).children().find('.responsive-image-wrapper').find('img').attr("src");
+        var title = $(element).children().find('.buzzer-title').text();
+        var publishInfo = $(element).children().find('.buzzer-details').find('.buzzer-pubdate').text();
+        var link = $(element).children().find('.buzzer-title-link').attr('href');
+
+        var mlbArticle = new Baseball({
+          image: img,
+          title: title,
+          publishInfo: publishInfo,
+          link: link
+        });
+
+        mlbArticle.save(function(err, document){
+          if(err) {
+            return res.send("ERROR: " + err);
+          } else {
+            console.log("nba!")
+          }
+        });
+      });
+    }// end if statement
+  });// end request
+  mongoose.model('Baseball').find(function(err, docs) {
+    if (!err) {
+      res.send(docs);
+    }
+  });
+});// end get route
 
 
 
