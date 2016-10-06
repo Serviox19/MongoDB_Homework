4d27271b3760f602cba0d9022a86a75870b848f3
Servio Mora <servio.mora19@gmail.com>
2016-10-06 16:22:55 -0400

    new banner

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   $('.button-collapse').sideNav({
-      menuWidth: 150, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
+    menuWidth: 150, // Default is 240
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
+
+  $('.carousel.carousel-slider').carousel({full_width: true});
 });
-
-
